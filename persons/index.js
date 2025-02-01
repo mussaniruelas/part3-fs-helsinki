c034caeb0270d536ab048f576c4f478b6ce52f92
Mussani Ruelas <mussaniruelas222@gmail.com>
2025-02-01 10:05:15 -0400

    3.1: Backend de la Agenda Telefónica paso 1 y 3.2: Backend de la Agenda Telefónica, paso 2

--- persons/index.js
+++ persons/index.js
@@ -4,16 +4,18 @@ app.use(express.json());
 
 const persons = require('./data.js');
 
-
 app.get('/', (req, res) =>{
     res.send('<h1>Hello World!</h1>');
+});
+
+app.get('/info', (req, res) =>{
+    res.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>`);
 });
 
 app.get('/api/persons', (req, res)=>{
     res.json(persons);
 })
-
-
 
 // -------------- Inicio --------------
 
